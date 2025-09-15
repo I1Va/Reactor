52557562e588beba76ea179633a3c03cb24b71eb
Abrutin Ivan Dmitrievich <mai.abrutinid@gmail.com>
2025-09-16 01:01:12 +0300

    molecule classes fix

--- inc/reactorcore.h
+++ inc/reactorcore.h
@@ -35,31 +35,7 @@ private:
         mass(mass),
         color(color) {};
 
-    virtual void collide(const Molecule &other, std::list<Molecule> &reactionResult) { 
-        bool thisCirclitState = strncmp(typeid(*this).name(), "Circlit", MAX_CLASS_NAME_LEN);
-        bool otherCirclitState = strncmp(typeid(other).name(), "Circlit", MAX_CLASS_NAME_LEN);
-
-        gm_vector<double, 2> collideCenter = position + (other.position - position) * 0.5;
-
-        if ((thisCirclitState ^ otherCirclitState) || (thisCirclitState && otherCirclitState)) {
-            int newMass = mass + other.mass;
-            gm_vector<double, 2> newMoveVector = (moveVector * mass + other.moveVector * other.mass) * (1.0 / newMass);
-            
-            Quadrit newMolecule(collideCenter, newMoveVector, newMass);
-            reactionResult.push_back(newMolecule);        
-        } else {
-            gm_vector<double, 2> boomCenter = position + (other.position - position) * 0.5;
-            int boomMoleculesCount = mass + other.mass;
-            double newRadius = Circlit::MIN_RADIUS * std::sin(std::numbers::pi / boomMoleculesCount);
-            double boomRootationAngle = 2 * std::numbers::pi / boomMoleculesCount;
-
-            gm_vector<double, 2> boomCurMoveVector = gm_vector<double, 2>(0, -1);
-            for (int i = 0; i < boomMoleculesCount; i++) {
-                Circlit newMolecule(collideCenter + boomCurMoveVector, boomCurMoveVector, 1);
-                reactionResult.push_back(newMolecule);
-            }
-        }
-    }
+    virtual void collide(const Molecule &other, std::list<Molecule> &reactionResult);
 };
 
 class Circlit : public Molecule {
@@ -75,6 +51,7 @@ public:
         gm_vector<unsigned char, 3> color=CIRCLIT_COLOR
     ): 
         Molecule(position, moveVector, mass, color) {}
+    
 };
 
 class Quadrit : public Molecule {
@@ -88,6 +65,8 @@ public:
         gm_vector<unsigned char, 3> color=QUADRIT_COLOR
     ): 
         Molecule(position, moveVector, mass, color) {}
+
+
 };
 
 
--- src/reactorcore.cpp
+++ src/reactorcore.cpp
@@ -1,1 +1,25 @@
 #include "reactorcore.h"
+
+void Molecule::collide(const Molecule &other, std::list<Molecule> &reactionResult) { 
+    bool thisCirclitState = typeid(*this) == typeid(Circlit);
+    bool otherCirclitState = typeid(other) == typeid(Circlit);
+
+    gm_vector<double, 2> collideCenter = position + (other.position - position) * 0.5;
+
+    if ((thisCirclitState ^ otherCirclitState) || (thisCirclitState && otherCirclitState)) {
+        int newMass = mass + other.mass;
+        gm_vector<double, 2> newMoveVector = (moveVector * mass + other.moveVector * other.mass) * (1.0 / newMass);
+        
+        reactionResult.push_back(Quadrit(collideCenter, newMoveVector, newMass));        
+    } else {
+        gm_vector<double, 2> boomCenter = position + (other.position - position) * 0.5;
+        int boomMoleculesCount = mass + other.mass;
+        double newRadius = Circlit::MIN_RADIUS * std::sin(std::numbers::pi / boomMoleculesCount);
+        double boomRootationAngle = 2 * std::numbers::pi / boomMoleculesCount;
+        gm_vector<double, 2> boomCurMoveVector = gm_vector<double, 2>(0, -1);
+    
+        for (int i = 0; i < boomMoleculesCount; i++) {
+            reactionResult.push_back(Circlit(collideCenter + boomCurMoveVector, boomCurMoveVector, 1));
+        }
+    }
+}
